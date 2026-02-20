d51f47a4fe2ecc62215c0b2c08f1301c783efea6
zeezaah <nazzauraaizachacha@gmail.com>
2026-02-20 19:42:42 +0700

    tuning 073

--- include/fufufuzizizi.h
+++ include/fufufuzizizi.h
@@ -12,7 +12,7 @@ extern float gxrs;
 
 Fuzzy *flipcuy = new Fuzzy();
 
-#define KROLL_BASE 3.48f
+#define KROLL_BASE 3.70f
 #define KP_BASE 3.00f
 float delta_kroll = 0.0f;
 float delta_kp    = 0.0f;
